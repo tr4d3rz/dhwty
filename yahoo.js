bd07ddf762474e1f95ab4397957d82c33cbda75e
simpego81 <pegoraro.simone.1981@gmail.com>
2017-05-06 18:09:27 +0200

    continua il deploy per Heroku

--- yahoo.js
+++ yahoo.js
@@ -1,5 +1,24 @@
 var util = require('util');
 var yahooFinance = require('yahoo-finance');
+var express = require('express');
+var app = express();
+var nCnt = 0;
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+
+app.get('/', function(request, response) {
+  console.log("--> Request main page");
+  response.send('OK !!!!Test da GITHUB.COM!!!' + nCnt);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 var SYMBOL = 'AAPL';
 
